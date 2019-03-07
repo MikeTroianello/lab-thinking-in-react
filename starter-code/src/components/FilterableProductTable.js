8ec7ca6ca0c5b9be3f163deb6552e6fb884ffd8e
Mike <troianello@protonmail.com>
2019-03-07 11:32:45 -0500

    :(

---
+++ starter-code/src/components/FilterableProductTable.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import ProductRow from './ProductRow'
+import ProductTable from './ProductTable'
+import SearchBar from './SearchBar'
+
+class FilterableProductTable extends Component {
+
+}
